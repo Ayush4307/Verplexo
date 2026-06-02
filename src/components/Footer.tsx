26f88fe76bfad97f533e7bf7f449e9800db18077
Ayush4307 <singhayush4307@gmail.com>
2026-06-02 20:58:31 +0530

    feat: add Footer component with brand labels

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+  return (
+    <footer className="site-footer">
+      <span>&copy; {new Date().getFullYear()} Verplexo</span>
+      <span>Modern software development for ambitious teams</span>
+    </footer>
+  );
+};
