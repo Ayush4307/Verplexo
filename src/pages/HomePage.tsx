60301ce5e1080b5da286bd17757a86f65f844278
Ayush4307 <singhayush4307@gmail.com>
2026-06-14 12:16:25 +0530

    feat: add About and Contact to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,15 @@
 import { HeroSection } from '../components/home/HeroSection'
 import { ServicesGrid } from '../components/home/ServicesGrid'
+import { AboutSection } from '../components/home/AboutSection'
+import { ContactForm } from '../components/home/ContactForm'
 
 export function HomePage() {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
       <ServicesGrid />
+      <ContactForm />
     </div>
   )
 }
