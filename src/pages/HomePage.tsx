1493e1858e4784f8646dae6d8c00474336369020
Ayush4307 <singhayush4307@gmail.com>
2026-06-14 12:14:30 +0530

    feat: assemble HomePage component

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from '../components/home/HeroSection'
+import { ServicesGrid } from '../components/home/ServicesGrid'
+
+export function HomePage() {
+  return (
+    <div>
+      <HeroSection />
+      <ServicesGrid />
+    </div>
+  )
+}
